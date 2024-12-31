7e213e4ca7c86d8d434f60bb2015a043a0e5a57c
benrobson <shadowolfbusiness@gmail.com>
2024-12-31 14:30:40 +1100

    Commit base template.

---
+++ src/main/java/me/benrobson/blockvault/Blockvault.java
@@ -0,0 +1,17 @@
+package me.benrobson.blockvault;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Blockvault extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
